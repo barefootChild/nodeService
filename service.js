cc95a55fa923fc17837b93952eb95f2047d51e82
zhaoyongsheng <zhaoyongsheng@eluying.com>
2017-02-13 15:18:00 +0800

    feat: modify some code

--- service.js
+++ service.js
@@ -5,6 +5,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
+    response.write("欢迎来到私人网站！");
     response.end();
 }).listen(8888);
