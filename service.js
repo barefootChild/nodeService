b842645a7f6a8ebb9502d06a648ae9c3bfebffb8
zhaoyongsheng <zhaoyongsheng@eluying.com>
2017-02-16 16:13:50 +0800

    feat: add new feat

--- service.js
+++ service.js
@@ -2,9 +2,35 @@
  * Created by zhaoyongsheng on 17/2/13.
  */
 var http = require("http");
+var path = require("path");
+var fs = require("fs");
 
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("欢迎来到私人网站！");
-    response.end();
+var mimeTypes = {
+    '.js' : 'text/javascript',
+    '.html': 'text/html',
+    '.css' : 'text/css'
+};
+
+http.createServer(function (request, response) {
+    var lookup = path.basename(decodeURI(request.url)) || 'index.html',
+        f = 'game/' + lookup;
+    //文件是否存在
+    fs.exists(f, function (exists) {
+        if (exists) {
+            fs.readFile(f, function (err, data) {
+                if (err) { response.writeHead(500);
+                    response.end('Server Error!');
+                    return;
+                }
+                //path.extname('about.html') --> .html
+                var headers = {'Content-type': mimeTypes[path.extname(lookup)]};
+                response.writeHead(200, headers);
+                response.end(data);
+            });
+            return false;
+        }
+        response.writeHead(404); //no such file found!
+        response.write("欢迎来到私人网站！");
+        response.end();
+    });
 }).listen(8888);
